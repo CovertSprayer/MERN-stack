c2c0194deb6810089026d71f26d1f2cc9afc7b30
Shreyance <shreyancebodh2013@gmail.com>
2023-02-27 19:33:14 +0530

    add Home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <p>Hello from Home</p>
+      <div className="home-page">
+        <div className="home-div">
+          <p>WELCOME</p>
+          <h1>This is MERN stack Test App</h1>
+        </div>
+      </div>
     </>
   )
 }
